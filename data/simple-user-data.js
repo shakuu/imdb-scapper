1e9c39f39d47eea7c2dfedf87ee49b6fbb38baa2
shakuu <todorob.h@gmail.com>
2016-11-19 14:02:06 +0200

    added find simple movies from MongoDb with pagination

---
+++ data/simple-user-data.js
@@ -0,0 +1,37 @@
+const SimpleMovie = require("../models/simple-movie-model");
+
+function findByName(name) {
+    const promise = new Promise((resolve, reject) => {
+        SimpleMovie.find({ name }, (err, response) => {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(response);
+        });
+    });
+
+    return promise;
+}
+
+function findPage(page, pageSize) {
+    const promise = new Promise((resolve, reject) => {
+        SimpleMovie.find({})
+            .skip(page * pageSize)
+            .limit(pageSize)
+            .exec((err, response) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(response);
+            });
+    });
+
+    return promise;
+}
+
+module.exports = {
+    findByName,
+    findPage
+};
