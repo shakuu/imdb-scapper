21f26164f22ea66f987288656ff35ccf5feeea9d
shakuu <todorob.h@gmail.com>
2016-11-19 17:33:55 +0200

    scrapper update

--- scrappers/detailed-movie-scrapper.js
+++ scrappers/detailed-movie-scrapper.js
@@ -2,14 +2,31 @@ const simpleUserData = require("../data/simple-user-data");
 const urlQueueProvider = require("../utils/url-queue-provider");
 const logger = require("../utils/file-logger");
 
-function getDetailedMovies() {
+const httpRequester = require("../utils/http-requester");
+
+function getDetailedMovies(options) {
+    options.pageSize = options.pageSize || 10;
+    options.pagesCount = options.pageNumber || 0;
+
     simpleUserData.findPage(1, 10)
         .then((movie) => {
             console.log(movie);
             return urlQueueProvider.getUrlQueueFromSimpleMovies(movie);
         })
         .then((urlQueue) => {
-            console.log(urlQueue);
+            while (!urlQueue.isEmpty()) {
+                getDetailedMovieFromImdbUrl(urlQueue.pop());
+            }
+        })
+        .catch((err) => {
+            logger.logError(err);
+        });
+}
+
+function getDetailedMovieFromImdbUrl(url) {
+    httpRequester.get(url)
+        .then((result) => {
+            const html = result.body;
         })
         .catch((err) => {
             logger.logError(err);
